21f9ab0b80fb69b9d2e97bd353e6c0eb86373d58
Mathyosa <teufofo.teu126@gmail.com>
2025-05-12 18:21:05 +0000

    Fase 1

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,49 +1,34 @@
 #include <stdio.h>
 
 int main() {
+    char linha[10] = {'A', 'B','C','D','E','F','G','H','I','J'};
+    int tabuleiro[10][10];
 
-   char linha[10] = {'A', 'B','C','D','E','F','G','H','I','J'};
-   int coluna = 10; // Número de colunas
-   int tabuleiro[10][coluna]; 
-
-   // Tabuleiro inicializado com zeros
-   for (int i = 0; i < 10; i++) {
-       for (int j = 0; j < 10; j++) {
-           tabuleiro[i][j] = 0;
-       }
-
-    }
-
-    // Preenche o tabuleiro com números de 1 a 10
-
+    // Inicializa o tabuleiro com zeros
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = (i * coluna) + (j + 0);
+            tabuleiro[i][j] = 0;
         }
     }
-    // Imprime o tabuleiro na tela
 
-    printf("  ");
-    for (int j = 0; j <= 10; j++) {
-        printf("%c ", linha[j]);
+    // Imprime cabeçalho das colunas
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", linha[j]);
     }
     printf("\n");
 
+    // Imprime o tabuleiro com valores 0
     for (int i = 0; i < 10; i++) {
-        printf("%d ", i + 1); // Imprime o número da linha
+        printf("%2d|", i + 1); // Imprime o número da linha
         for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
+            printf(" %d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-    printf("\n");
-    printf("Tabuleiro BATALHA NAVAL");
-    printf("\n");
 
-   
+    printf("\nTabuleiro BATALHA NAVAL\n");
 
-
-   
     return 0;
 }
 
