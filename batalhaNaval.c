34e910aba729a81705d99f6e82220d084c0972dc
Mathyosa <teufofo.teu126@gmail.com>
2025-05-13 19:51:27 +0000

    Fase 2

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -29,6 +29,43 @@ int main() {
 
     printf("\nTabuleiro BATALHA NAVAL\n");
 
+    //Posiciona os navios horizontais
+    
+    tabuleiro[0][0] = 3; // Navio 1
+    tabuleiro[0][1] = 3; // Navio 1
+    tabuleiro[0][2] = 3; // Navio 1
+    tabuleiro[1][0] = 3; // Navio 2
+    tabuleiro[1][1] = 3; // Navio 2
+    tabuleiro[1][2] = 3; // Navio 2
+
+    //Posiciona os navios diagonais
+    
+    tabuleiro[3][1] = 3; // Navio 3
+    tabuleiro[4][2] = 3; // Navio 3
+    tabuleiro[5][3] = 3; // Navio 3
+    
+    tabuleiro[9][3] = 3;  // Navio 4
+    tabuleiro[8][4] = 3; // Navio 4
+    tabuleiro[7][5] = 3; // Navio 4
+    
+
+
+
+    // Imprime o tabuleiro com os navios
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", linha[j]);
+    }
+    printf("\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%2d|", i + 1); // Imprime o número da linha
+        for (int j = 0; j < 10; j++) {
+            printf(" %d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
     return 0;
 }
 
